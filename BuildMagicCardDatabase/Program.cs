58da3990c9d6768cd495402d75c98f70fdfd9dd1
Gyasi Grey <gyasi.grey@gmail.com>
2019-07-09 20:20:40 -0400

    Initial Commit

---
+++ BuildMagicCardDatabase/Program.cs
@@ -0,0 +1,28 @@
+﻿using MtgApiManager.Lib.Core;
+using MtgApiManager.Lib.Model;
+using MtgApiManager.Lib.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildMagicCardDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardService service = new CardService();
+            Exceptional<List<Card>> result = service.All();
+            if(result.IsSuccess)
+            {
+                var value = result.Value;
+            }
+            else
+            {
+                var exception = result.Exception;
+            }
+        }
+    }
+}
