2702b0d2ce1efc881d9b42c6afeb30beecb305a6
Gyasi Grey <gyasi.grey@murphyandco.com>
2019-07-09 21:07:18 -0400

    Added stuff to put it in the database

--- BuildMagicCardDatabase/Program.cs
+++ BuildMagicCardDatabase/Program.cs
@@ -1,4 +1,5 @@
-﻿using MtgApiManager.Lib.Core;
+﻿using AccessLayer;
+using MtgApiManager.Lib.Core;
 using MtgApiManager.Lib.Model;
 using MtgApiManager.Lib.Service;
 using System;
@@ -13,11 +14,58 @@ namespace BuildMagicCardDatabase
     {
         static void Main(string[] args)
         {
+            Database db = new Database();
+
             CardService service = new CardService();
             Exceptional<List<Card>> result = service.All();
             if(result.IsSuccess)
             {
                 var value = result.Value;
+
+                foreach(Card c in value)
+                {
+                    DBParameter[] parameters = new DBParameter[]
+                    {
+                        new DBParameter()
+                        {
+                            name = "@Name",
+                            type = System.Data.SqlDbType.VarChar,
+                            value = c.Name
+                        },
+                        new DBParameter()
+                        {
+                            name = "@RulesText",
+                            type = System.Data.SqlDbType.VarChar,
+                            value = c.Text
+                        },
+                        new DBParameter()
+                        {
+                            name = "@CMC",
+                            type = System.Data.SqlDbType.Int,
+                            value = c.Cmc
+                        },
+                        new DBParameter()
+                        {
+                            name = "@Power",
+                            type = System.Data.SqlDbType.Int,
+                            value = c.Power
+                        },
+                        new DBParameter()
+                        {
+                            name = "@Toughness",
+                            type = System.Data.SqlDbType.Int,
+                            value = c.Toughness
+                        },
+                        new DBParameter()
+                        {
+                            name = "@Type",
+                            type = System.Data.SqlDbType.VarChar,
+                            value = c.Type
+                        }
+                    };
+
+                    db.nonquery("INSERT INTO Cards Name, RulesText, CMC, Power, Toughness, Type VALUES (@Name, @RulesText, @CMC, @Power, @Toughness)", parameters);
+                }
             }
             else
             {
