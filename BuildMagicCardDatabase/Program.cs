9495fd341b5271c8f3146aa2c5568107d229a876
Gyasi Grey <gyasi.grey@gmail.com>
2019-07-09 21:42:52 -0400

    Added MultiverseId

--- BuildMagicCardDatabase/Program.cs
+++ BuildMagicCardDatabase/Program.cs
@@ -16,60 +16,77 @@ namespace BuildMagicCardDatabase
         {
             Database db = new Database();
 
+            int PageNum = 1;
             CardService service = new CardService();
-            Exceptional<List<Card>> result = service.All();
-            if(result.IsSuccess)
+            Console.WriteLine("Getting Page " + PageNum);
+            Exceptional<List<Card>> result = service.Where(X => X.Page, PageNum).All();
+
+            while (result.IsSuccess)
             {
                 var value = result.Value;
 
                 foreach(Card c in value)
                 {
-                    DBParameter[] parameters = new DBParameter[]
+                    //If the MultiverseId is NULL that means the card is not on Gatherer.
+                    //This only happens with a small number of reprint sets so it won't matter for this project
+                    if (c.MultiverseId != null)
                     {
-                        new DBParameter()
+                        DBParameter[] parameters = new DBParameter[]
                         {
-                            name = "@Name",
-                            type = System.Data.SqlDbType.VarChar,
-                            value = c.Name
-                        },
-                        new DBParameter()
-                        {
-                            name = "@RulesText",
-                            type = System.Data.SqlDbType.VarChar,
-                            value = c.Text
-                        },
-                        new DBParameter()
-                        {
-                            name = "@CMC",
-                            type = System.Data.SqlDbType.Int,
-                            value = c.Cmc
-                        },
-                        new DBParameter()
-                        {
-                            name = "@Power",
-                            type = System.Data.SqlDbType.Int,
-                            value = c.Power
-                        },
-                        new DBParameter()
-                        {
-                            name = "@Toughness",
-                            type = System.Data.SqlDbType.Int,
-                            value = c.Toughness
-                        },
-                        new DBParameter()
-                        {
-                            name = "@Type",
-                            type = System.Data.SqlDbType.VarChar,
-                            value = c.Type
-                        }
-                    };
+                            new DBParameter()
+                            {
+                                name = "@Name",
+                                type = System.Data.SqlDbType.VarChar,
+                                value = c.Name
+                            },
+                            new DBParameter()
+                            {
+                                name = "@RulesText",
+                                type = System.Data.SqlDbType.VarChar,
+                                value = c.Text
+                            },
+                            new DBParameter()
+                            {
+                                name = "@CMC",
+                                type = System.Data.SqlDbType.Int,
+                                value = c.Cmc
+                            },
+                            new DBParameter()
+                            {
+                                name = "@Power",
+                                type = System.Data.SqlDbType.VarChar,
+                                value = c.Power
+                            },
+                            new DBParameter()
+                            {
+                                name = "@Toughness",
+                                type = System.Data.SqlDbType.VarChar,
+                                value = c.Toughness
+                            },
+                            new DBParameter()
+                            {
+                                name = "@Type",
+                                type = System.Data.SqlDbType.VarChar,
+                                value = c.Type
+                            },
+                            new DBParameter()
+                            {
+                                name = "@MultiverseId",
+                                type = System.Data.SqlDbType.Int,
+                                value = c.MultiverseId
+                            }
+                        };
 
-                    db.nonquery("INSERT INTO Cards Name, RulesText, CMC, Power, Toughness, Type VALUES (@Name, @RulesText, @CMC, @Power, @Toughness)", parameters);
+                        Console.WriteLine("Inserting " + c.Name);
+                        db.nonquery("INSERT INTO Cards (Name, RulesText, CMC, Power, Toughness, Type, MultiverseId) VALUES (@Name, @RulesText, @CMC, @Power, @Toughness, @Type, @MultiverseId)", parameters);
+                    }
                 }
-            }
-            else
-            {
-                var exception = result.Exception;
+
+                //Increment the Page
+                PageNum++;
+                Console.WriteLine("Getting Page " + PageNum);
+                //Get the next page
+                result = service.Where(X => X.Page, PageNum).All();
             }
         }
     }
